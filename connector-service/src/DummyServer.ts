a2c1f714ef1aae7dfbd9e9f8763bb42c06ee90b3
Francisco Enzo Di Giorgio <f.e.digiorgio@gmail.com>
2024-05-27 00:56:38 -0300

    DummyServer returns a dummy response.

--- connector-service/src/DummyServer.ts
+++ connector-service/src/DummyServer.ts
@@ -1,14 +1,16 @@
-import net from "net";
+import * as http from 'http';
 
-const dummyServer: net.Server = net.createServer((socket: net.Socket) => {
-    socket.on('data', (data: Buffer) => {});
+const PORT: number = 3000;
 
-    socket.on('end', () => {});
+const server: http.Server = http.createServer((req: http.IncomingMessage, res: http.ServerResponse) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('This a dummy response');
 });
 
+
 export function listenDummyServer() {
-    const port = 3000
-    dummyServer.listen(port, () => {
-        console.log(`Listen port: ${port}`);
+    server.listen(PORT, () => {
+        console.log(`Listen ${PORT}`);
     });
 }
+
