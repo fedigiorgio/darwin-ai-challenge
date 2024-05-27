6c1265db51d510ccccbde3d07aa866fd8870f420
Francisco Enzo Di Giorgio <f.e.digiorgio@gmail.com>
2024-05-26 22:21:15 -0300

    Remove main.py

--- bot-service/src/main.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-from src import app
-
-logging.basicConfig(level='INFO', format='%(asctime)s - %(levelname)s - %(message)s')
-
-if __name__ == '__main__':
-
-    app.run(debug=True)
